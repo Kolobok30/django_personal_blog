8d1e73b0f3ce84d69c56740086b0db88c09438f7
Юрий Буковский <l7361@yandex.ru>
2023-04-21 11:50:22 +0300

    full

---
+++ README.md
@@ -0,0 +1,75 @@
+# Personal blog using Django
+  - Build data models, views, and URLs
+  - Implement an administration site for your blog
+  - Use canonical URLs for modles and implement SEO-friendly URLs for posts
+  - Build post pagination and learn how to create class-based views
+  - Use forms to allow readers to share posts via email and implement a comment system using model forms
+  - Add tags to posts using [django-taggit](https://github.com/jazzband/django-taggit) and recommend similar posts based on shared tags
+  - Implement custom template tags to display latest posts and most commented posts
+  - Implement a custom template filter to render [Markdown](https://github.com/Python-Markdown/markdown)
+  - Create a sitemap and a RSS feed for your blog
+  - Implement a full-text search engine using PostgreSQL
+
+## Start project using Docker
+1. The first thing to do is to clone the repository:
+    ```
+    git clone https://github.com/Kolobok30/django_personal_blog.git
+   ```
+2. Using the Dockerfile and docker-compose.yaml run the project:
+   ```
+   docker-compose up --build
+   ```
+3. In new terminal window run command:
+   ````
+   docker-compose exec web sh
+   ````
+4. Run the migrations in the container, create a superuser, and populate the database with initial data:
+   ````
+   python manage.py migrate
+   
+   python manage.py createsuperuser
+   
+   python3 manage.py loaddata mysite_data.json
+   ````
+
+
+
+## Start the project without Docker:
+1. The first thing to do is to clone the repository:
+    ```
+    git clone https://github.com/Kolobok30/django_personal_blog.git
+    
+    cd mysite
+    ```
+2. Create a virtual environment to install dependencies in and activate it:
+    ```
+    python3 -m venv venv
+   
+    source venv/bin/activate
+    ```   
+3. Then install the dependencies:
+    ```
+   (env)$ pip install -r requirements.txt
+    ```
+    &emsp; Note the (env) in front of the prompt.
+
+    &emsp; This indicates that this terminal session operates in a virtual environment set.
+
+
+4. Once pip has finished downloading the dependencies make migrations:
+    ```
+    python3 manage.py migrate
+    ```
+5. Create superuser:
+    ````
+    python3 manage.py createsuperuser
+    ````
+   
+6. Load testing data:
+    ````
+    python3 manage.py loaddata mysite_data.json
+    `````
+
+7. And finally run test server:
+    ````
+    (env)$ python3 manage.py runserver
---
+++ blog/views.py
@@ -0,0 +1,125 @@
+from django.db.models import Count
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
+from django.views.generic import ListView
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+from .models import Post
+from .forms import EmailPostForm, CommentForm, SearchForm
+from taggit.models import Tag
+from django.contrib.postgres.search import TrigramSimilarity
+
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page_number is not an integer deliver first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page_number is out of range deliver last page
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html',
+                  {'posts': posts, 'tag': tag})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day
+                             )
+    post_tags_id = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_id). \
+        exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+                        .order_by('-same_tags', '-publish')[0:4]
+    form = CommentForm()
+    comments = post.comments.filter(active=True)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'form': form,
+                   'comments': comments,
+                   'similar_posts': similar_posts})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    template_name = 'blog/post/list.html'
+    paginate_by = 3
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = EmailPostForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                (post.get_absolute_url()))
+            subject = f"{cd['name']} recommends you to read" \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']} \'s comments: {cd['comments']}"
+            send_mail(subject, message, cd['email'],
+                      [cd['to']],
+                      fail_silently=False, )
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'form': form,
+                                                    'post': post,
+                                                    'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post, 'form': form, 'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            # search_vector = SearchVector('title', weight='A') + \
+            #                 SearchVector('body', weight='B')
+            # search_query = SearchQuery(query)
+            # results = Post.objects. \
+            #     annotate(search=search_vector,
+            #              rank=SearchRank(search_vector, search_query)). \
+            #     filter(rank__gte=0.3).order_by('-rank')
+            results = Post.objects.\
+                annotate(similarity=TrigramSimilarity('title', query)).\
+                filter(similarity__gt=0.1).order_by('-similarity')
+    return render(request, 'blog/post/search.html',
+                  {'form': form, 'query': query, 'results': results})
